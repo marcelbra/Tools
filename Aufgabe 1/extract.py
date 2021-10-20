7715d70e928a7c794665d33eaa75112b69acbe3a
marcelbra <marcelbraasch@gmail.com>
2021-10-20 15:54:42 +0200

    Initial commit

--- Aufgabe 1/extract.py
+++ Aufgabe 1/extract.py
@@ -7,23 +7,42 @@ from html.parser import HTMLParser
 
 
 class HTMLToText(HTMLParser):
-    plain_text = ""
 
-    def handle_data(self, data):
-        # Filtern nach <title> und <p>? Index Files ausschließen?
-        if self.correct_tag:
-            self.plain_text += data
+    def __init__(self):
+        super(HTMLToText, self).__init__()#)convert_charrefs=True)
+        self.docs = []
+
+    def dataloader(self,
+                   path="/Users/marcelbraasch/Desktop/Tools"
+                   ):
+
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                if file.endswith(".html") and not file == "index.html":
+                    with open(os.path.join(root, file)) as f:
+                        html = f.read()
+                        #print(" ".join(self.docs[0].plain_text.split()))
+                        #self.docs.append(html)
+                        return html
+
+    def get_doc_by_path(self,
+                        path="/Users/marcelbraasch/Desktop/Tools/www.tagesschau.de/wirtschaft/verbraucher/tanktourismus-eu-benzinpreise-101.html"
+                        ):
+        with open(path) as f:
+            html = f.read()
+            return html
+
+    def get_paragraph(self):
+        html = self.get_doc_by_path()
+        pattern = r"(.*)</p>"
+        paragraphs = re.findall(pattern, html)
+        filtered = []
+        for paragraph in paragraphs:
+
+
+        s = 0
+
 
 
 html_to_text = HTMLToText()
-
-file_path = "./data/aufgabe1/www.tagesschau.de/"
-
-for root, dirs, files in os.walk(sys.argv[1]):
-    for file in files:
-        if file.endswith("fluechtlinge-101.html") and not file == "index.html":
-            with open(os.path.join(root, file)) as f:
-                html = f.read()
-                html_to_text.feed(html)
-
-print(" ".join(html_to_text.plain_text.split()))
+html_to_text.get_paragraph()
