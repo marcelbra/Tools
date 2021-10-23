a8a402b980e28199c82a607e5242573593bfeae9
nseeberg <92861898+nseeberg@users.noreply.github.com>
2021-10-23 15:57:33 +0200

    Add files via upload
    Added tokenization features:
    sentence marking
    tokenization markers

--- Aufgabe 1/tokenize.py
+++ Aufgabe 1/tokenize.py
@@ -2,63 +2,41 @@
 
 import re
 import string
-import sys
 
 
 class HTMLTokenizer:
 
-    def __init__(self, abbreviations, in_file, out_file):
+    def __init__(self, abbreviations, in_file):
         with open(abbreviations, 'r', encoding='utf-8') as ab:
             self.abv = ab.read()
+        # self.abv = abbreviations
         self.infile = in_file
-        self.outfile = out_file
 
-    def read_file(self):
-        text = (open(self.infile, "r")).read()
+    def read_file(self, file="text.txt"):
+        text = open(file, "r").read()
         return text
 
-    def sentence_split(self):
+    def sentence_split(self, text):
         sent_pattern = r"(?<=[a-zäöü][.!?])\s+"
-        sentences = re.split(sent_pattern, self.read_file())
-        return "\n".join(sentences)
+        sentences = re.split(sent_pattern, infile)
+        return sentences
 
-    def tokenize_text(self):
-        text = self.read_file()
-        tokenizer_pattern = r"([\w\s]+[\.\?\!]+)"
+    def tokenize_sentences(self, sentence_list):
+        new_sents = []
+        for sent in sentences:
+            for pos, char in enumerate(sent):
+                if char in string.punctuation:
+                    sent = sent.replace(char, " " + char + " ")
+            new_sents.append(sent)
 
-        # TODO: Satz endet immer nach Punkt außer bei Abkürzungen und zwischen Zahlen?
-        #  An Fragezeichen und Ausrufezeichen denken!
-
-        tokenized = re.findall(tokenizer_pattern, text)
-
-        for tok in tokenized:
-            for a in self.abv:
-                if tok == a:
-                    tok = tok.replace(tok, self.abv)
-
-        return tokenized
-
-    def save_text(self, sentences):
-        with open(self.outfile, "w") as f:
-            f.write(sentences)
+        marked_sentences = ["<s>" + sent + "</s>" + "\n" for sent in new_sents]
+        for marked_sent in marked_sentences:
+            print("|".join(marked_sent.split()))
 
 
-tokenizer = HTMLTokenizer("abbreviations.txt", "text.txt", "text.tok")
-text_tok = tokenizer.tokenize_text()
-sentences = tokenizer.sentence_split()
-#print(sentences)
-
-tokenizer.save_text(sentences)
-
-
-
-
-
-
-
-
-
-
-
+tokenizer = HTMLTokenizer("abkuerzungen.txt", "text.txt")
+infile = tokenizer.read_file()
+sentences = tokenizer.sentence_split(infile)
+print(tokenizer.tokenize_sentences(sentences))
 
 
