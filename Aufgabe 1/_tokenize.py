56bb1cb2d238defb8f76c060a37f5946251799cd
marcelbra <marcelbraasch@gmail.com>
2021-10-25 00:45:11 +0200

    Minor changes for readability

--- Aufgabe 1/_tokenize.py
+++ Aufgabe 1/_tokenize.py
@@ -15,47 +15,38 @@ class HTMLTokenizer:
 
 
     def load_data(self):
+        """Loads abbreviations and input file."""
         with open(self.abrev_path, "r", encoding='utf-8') as f:
             self.abrev = set(f.read().split("\n"))
-
         with open(self.in_file_path, "r", encoding='utf-8') as f:
             self.text = f.read()
 
     def split_sentences(self):
         """
-        Replaces all dots which are not grammatical dots with "QU4K".
-        Then performs splitting by dots.
-        Sentences with e.g., "Dr.", "ca.", etc. will not be split because they are now "DrQU4K", "caQU4K"
-        Replace back the QU4Ks to dots.
-        :return:
+        Replaces all dots which are not grammatical dots with "QU4K". This includes 1) words which
+        are in the abbreviations list, 2) numbers and 3) internet addresses. Next, 4) Split by . , ! ? and
+        5) replace QU4K back to dot. This results in correct split since dots which are not grammatical
+        ones anymore are not considered for splitting.
         """
         text = "Es ist z.B. schön, dass ca. 1.000.000 andere der Kidner zw. Nepal und China beim Dr. Arzt lachen. Zudem kann man sagen es ist warm. Nicht wirklich kalt."
-        # text = self.text
 
-        # QU4K abbreviations
-        text = " ".join([word.replace(".", "QU4K") if word in self.abrev else word
+        text = " ".join([word.replace(".", "QU4K") if word in self.abrev else word  # 1)
                          for i, word in enumerate(text.split())])
-        # QU4K numbers
         number_pattern = r"(?<!\S)\d{1,3}(?:\.\d{3})*(?!\S)"
-        text = re.sub(number_pattern, lambda m: m.group().replace('.', 'QU4K'), text)
-
-        # QU4K internet addresses
+        text = re.sub(number_pattern, lambda m: m.group().replace('.', 'QU4K'), text)  # 2)
         address_pattern = r"(www).([A-Za-z0-9]*)\.(de|com|org)"
-        re.sub(address_pattern, r"\1QU4K\2QU4K\3", text)
-
-        # Split by . , ! ?
-        # Dots which are not denoting sentence end are now QU4Ks!
+        re.sub(address_pattern, r"\1QU4K\2QU4K\3", text)  # 3)
         split_pattern = r"(?<=[a-zäöü][.!?])\s+"
-        sentences = re.split(split_pattern, text)
-
-        # Turn QU4Ks back into dots
-        sentences = [sentence.replace("QU4K", ".") for sentence in sentences]
+        sentences = re.split(split_pattern, text)  # 4)
+        sentences = [sentence.replace("QU4K", ".") for sentence in sentences]  # 5)
         self.sentences = sentences
 
     def tokenize(self):
+        """Tokenizes sentences into singlue words."""
         self.tokenized = [sent.split(" ") for sent in self.sentences]
 
     def save_text(self):
+        """Saves the text to the specified output file."""
         s = ""
         for sentence in self.sentences:
             s += f"{sentence}\n"
