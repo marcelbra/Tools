8741899e6220914d666fd15396fbd025668689df
marcelbra <marcelbraasch@gmail.com>
2021-11-23 04:36:27 +0100

    Refactor vector operations

--- Aufgabe 5/model.py
+++ Aufgabe 5/model.py
@@ -7,21 +7,7 @@ from features import (
     length_of
     )
 import math
-
-def add(list_1, list_2):
-    return [x + list_2[i] for i, x in enumerate(list_1)]
-
-def sub(list_1, list_2):
-    return [x - list_2[i] for i, x in enumerate(list_1)]
-
-def mul(list_1, list_2):
-    return [x * list_2[i] for i, x in enumerate(list_1)]
-
-def div(list_1, list_2):
-    return [x / list_2[i] for i, x in enumerate(list_1)]
-
-def dot(list_1, list_2):
-    return sum(mul(list_1, list_2))
+from vector_operations import add, sub, mul, div, dot
 
 
 class LogLinear:
---
+++ Aufgabe 5/vector_operations.py
@@ -0,0 +1,14 @@
+def add(list_1, list_2):
+    return [x + list_2[i] for i, x in enumerate(list_1)]
+
+def sub(list_1, list_2):
+    return [x - list_2[i] for i, x in enumerate(list_1)]
+
+def mul(list_1, list_2):
+    return [x * list_2[i] for i, x in enumerate(list_1)]
+
+def div(list_1, list_2):
+    return [x / list_2[i] for i, x in enumerate(list_1)]
+
+def dot(list_1, list_2):
+    return sum(mul(list_1, list_2))
