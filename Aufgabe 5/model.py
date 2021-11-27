1d23a7bda43f40ee75df580508a343422ecf7800
marcelbra <marcelbraasch@gmail.com>
2021-11-27 16:00:59 +0100

    Fixed training bug

--- Aufgabe 5/model.py
+++ Aufgabe 5/model.py
@@ -1,10 +1,23 @@
+"""
+P7 Experimente, Evaluierung und Tools
+Aufgabe 5 - Spam classification using log linear models
+
+Group:
+Marcel Braasch
+Nadja Seeberg
+Sinem Kühlewind (geb. Demiraslan)
+"""
+
 from collections import defaultdict
 import sys
 import os
 from features import (
-    avg_word_length_of,
-    amount_exclamation_mark_of,
-    length_of
+    avg_word_length_pos,
+    avg_word_length_neg,
+    amount_exclamation_mark_pos,
+    amount_exclamation_mark_neg,
+    length_pos,
+    length_neg
     )
 import math
 from vector_operations import add, sub, mul, div, dot, create_vec
@@ -12,12 +25,12 @@ from vector_operations import add, sub, mul, div, dot, create_vec
 
 class LogLinear:
 
-    def __init__(self, mode):
+    def __init__(self, mode, paramfile, data_dir):
 
         # Set parameters
         self.mode = mode
-        self.paramfile = sys.argv[1]
-        self.data_dir = sys.argv[2]
+        self.paramfile = paramfile
+        self.data_dir = data_dir
         if mode == "train":
             self.data_dir, self.paramfile = self.paramfile, self.data_dir
         self.classes = ["ham", "spam"] #TODO: next(os.walk(self.data_dir))[1] # irgendwas spinnt hier bei mir
@@ -42,41 +55,38 @@ class LogLinear:
         if self.mode == "test": pass
 
         elif self.mode == "train":
-
             epochs = range(20)
+            grad = create_vec(0, self.n)
             for epoch in epochs:
-                grad = create_vec(0, self.n)
+                scores = create_vec(0, self.n)
                 for sample, true_class in self.data:
-
                     # Feature vector, left term in slides
-                    feature_score = self.feature_vec(sample, true_class)
-
+                    feature_score_left = self.feature_vec(sample, true_class)
                     # Expectation times feature vector, right term in slides
                     prob_class_given_doc = 0
+                    counter = 0  # Counts at which vector entry we are right now
                     for _class in self.classes:
-                        # This is vector of sum_c' p_theta (c',d) * f(c',d)
-                        Z = sum([math.exp(dot(self.theta, self.feature_vec(c, sample)))
-                                 for c in self.classes])
+                        # This is the part right of the expectation (see slides)
+                        feature_score_right = self.features_functions[counter](sample, _class)
+                        counter += 1
+                        # This is the normalization
+                        Z = sum([math.exp(dot(self.theta, self.feature_vec(c, sample))) for c in self.classes])
+                        # Unnormalized score
                         class_prob = math.exp(dot(self.theta, self.feature_vec(_class, sample)))
-                        prob_class_given_doc += class_prob / Z
-
-                    # Create vector of expectations times (element wise) feature vector
-                    expectation = create_vec(prob_class_given_doc, self.n)
-                    expectation_times_feature = mul(expectation, self.feature_vec(_class,sample))
-
+                        # Combine all terms
+                        prob_class_given_doc += (class_prob * feature_score_right) / Z
                     # Combine left and right term
                     score = sub(feature_score, expectation_times_feature)
-
-                    # Update gradient
-                    grad = add(grad, score)
-
+                    # Accumulate gradient
+                    scores = add(scores, score)
+                # Update gradient
+                grad = add(grad, scores)
                 # Update parameters
                 self.theta = add(self.theta, mul(create_vec(self.eta, n), grad))
 
                 # TODO: Evaluation / logging after every gradient update
-                # TODO: feature functions
-                # TODO: weight decay / hyperparameter tuning
-                # TODO: test SGD / batched gradient update
+                # TODO: Weight decay
+                # TODO: Test SGD / batched gradient update
 
     def feature_vec(self, _class, sample):
         return [ff(sample[i], _class) for i, ff in enumerate(self.features_functions)]
