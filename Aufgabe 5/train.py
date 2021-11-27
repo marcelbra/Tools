1531b3140ffde0d83a1220c36510b890ae41307f
marcelbra <marcelbraasch@gmail.com>
2021-11-27 16:00:03 +0100

    Fixed training

--- Aufgabe 5/train.py
+++ Aufgabe 5/train.py
@@ -3,6 +3,6 @@ import sys
 
 if __name__ == "__main__":
     assert len(sys.argv) == 3, "Call script as $ python3 test.py paramfile mail-dir"
-    ll = LogLinear(mode="train")
+    ll = LogLinear(mode="train", paramfile=sys.argv[1], data_dir=sys.argv[2])
     ll.fit()
     #ll.save_parameters()
