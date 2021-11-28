051b4830de79ce71c29b722da9d9b56d27de7c5a
marcelbra <marcelbraasch@gmail.com>
2021-11-28 14:25:18 +0100

    Refactoring

--- Aufgabe 5/test.py
+++ Aufgabe 5/test.py
@@ -3,6 +3,7 @@ import sys
 
 if __name__ == "__main__":
     assert len(sys.argv) == 3, "Call script as $ python3 test.py paramfile test-dir"
-    ll = LogLinear(mode="test", data_dir=sys.argv[2], paramfile=sys.argv[1])
-    ll.load_parameters()
-    ll.predict()
+    mode = "test"
+    ll = LogLinear(mode=mode, data_dir=sys.argv[2], paramfile=sys.argv[1])
+    params = ll.load_parameters()
+    ll.predict(mode="test", theta=params)
--- Aufgabe 5/train.py
+++ Aufgabe 5/train.py
@@ -4,5 +4,5 @@ import sys
 if __name__ == "__main__":
     assert len(sys.argv) == 3, "Call script as $ python3 train.py train-dir paramfile"
     ll = LogLinear(mode="train", data_dir=sys.argv[1], paramfile=sys.argv[2])
-    ll.fit()
-    ll.save_parameters()
+    params = ll.fit()
+    ll.save_parameters(params)
