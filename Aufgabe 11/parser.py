22a037c861f2013ceeabe0ba96110b289896b169
nseeberg <nadja.seeberg@web.de>
2022-01-27 20:20:17 +0100

    debugging

--- Aufgabe 11/parser.py
+++ Aufgabe 11/parser.py
@@ -11,33 +11,36 @@ import argparse
 import torch
 import torch.nn as nn
 from torch.nn import LSTM
+from data import Data
 
 
 class WordEncoder(nn.Module):
 
     def __init__(self, config):
         super().__init__()
-        self.embedding_layer = nn.Embedding(num_embeddings=config["num_prefixes"],
+        self.dropout = nn.Dropout(p=config["dropout"])
+        self.embedding_layer = nn.Embedding(num_embeddings=config["num_chars"],
                                             embedding_dim=config["embeddings_dim"])
-        self.embedding_layer = nn.Embedding(num_embeddings=config["num_suffixes"],
-                                            embedding_dim=config["embeddings_dim"])
+
         self.forward_lstm = nn.LSTM(batch_first=True,
                                     input_size=config["embeddings_dim"],
-                                    hidden_size=config["word_encoder_hidden_dim"],
-                                    dropout=config["word_encoder_lstm_dropout"])
+                                    hidden_size=config["word_encoder_hidden_dim"])
         self.backward_lstm = nn.LSTM(batch_first=True,
                                      input_size=config["embeddings_dim"],
-                                     hidden_size=config["word_encoder_hidden_dim"],
-                                     dropout=config["word_encoder_lstm_dropout"])
+                                     hidden_size=config["word_encoder_hidden_dim"])
 
     def forward(self, prefixes, suffixes):
+        #TODO: Richtiges Format übergeben
         prefixes = prefixes.to(torch.int64)
         suffixes = suffixes.to(torch.int64)
         pref = self.embedding_layer(prefixes)
+        pref = self.dropout(pref)
         prefix_repr, _ = self.forward_lstm(pref)
         suf = self.embedding_layer(suffixes)
+        suf = self.dropout(suf)
         suffix_repr, _ = self.backward_lstm(suf)
-        word_representation = torch.cat((suffix_repr, prefix_repr), dim=2)
+        word_representation = torch.cat((suffix_repr[:, -1],
+                                         prefix_repr[:, -1]), dim=2)
         return word_representation
 
 class SpanEncoder(nn.Module):
@@ -46,22 +49,24 @@ class SpanEncoder(nn.Module):
         self.bi_lstm = LSTM(batch_first=True,
                             bidirectional=True,
                             input_size=config["word_encoder_hidden_dim"]*2, # Because we concatenated
-                            hidden_size=config["span_encoder_hidden_dim"],
-                            dropout=config["span_encoder_lstm_dropout"])
+                            hidden_size=config["span_encoder_hidden_dim"])
 
     def forward(self, word_repr):
-        batch_size, length, dim = word_repr.size()
-        zeros = torch.zeros((batch_size,dim)).view(batch_size, 1, dim)
-        padded_word_repr = torch.cat((zeros, word_repr, zeros), dim=1)
+        sent_length, repr_size = word_repr.size()
+        zeros = torch.zeros((1, repr_size))
+        padded_word_repr = torch.cat((zeros, word_repr, zeros),
+                                     dim=1).unsqueeze(0)
         spans, _ = self.bi_lstm(padded_word_repr)
-        forward_repr, backward_repr = spans[:,:,:dim], spans[:,:,dim:]
-        r_iks = torch.empty((batch_size, 0, 2*dim))
-        for i in range(1, length+1):
-            forward = forward_repr[:,i:,:] - forward_repr[:,:-i,:]
-            backward = backward_repr[:,:-i,:] - backward_repr[:,i:,:]
-            span_repr = torch.cat((forward, backward), dim=2)
-            r_iks = torch.cat((r_iks, span_repr), dim=1)
-        return r_iks
+        forward_repr, backward_repr = spans.squeeze(0).split(dim, 1)
+
+        forward = forward_repr[:-1]  # letzte Repräsentation entfernen
+        backward = backward_repr[1:]  # erste Repräsentation entfernen
+
+        span_reprs = [torch.cat((forward[l:] - forward[:-l],
+                             backward[:-l] - backward[l]), -1)
+                  for l in range(1, sent_length)]
+        span_reprs = torch.cat(span_reprs)
+        return span_reprs
 
 class Parser(nn.Module):
     def __init__(self, config):
@@ -80,8 +85,34 @@ class Parser(nn.Module):
     def forward(self, prefix_ids, suffix_ids):
         word_repr = self.word_encoder(prefix_ids, suffix_ids)
         span_repr = self.span_encoder(word_repr)
-        span_label_scores = self.feedforward(span_representations)
+        span_label_scores = self.feedforward(span_repr)
         batch_size, no_constiutuents, _ = span_label_scores.size()
         zeros = torch.zeros((batch_size,no_constiutuents, 1))
         span_label_scores = torch.cat((span_label_scores, zeros), dim=2) # Adding 0 vektor for "no constituent" class
         return span_label_scores
+
+config = {"num_chars": 500,
+          "num_class": 10,
+          "embeddings_dim": 100,
+          "word_encoder_hidden_dim": 100,
+          "span_encoder_hidden_dim": 200,
+          "word_encoder_lstm_dropout": 0.1,
+          "span_encoder_lstm_dropout": 0.1,
+          "fc_dropout": 0.1,
+          "fc_hidden_dim": 32,
+          "batch_size": 32,
+          "lr": 1e-3,
+          "dropout": 0.1
+          }
+
+parser = Parser(config)
+data = Data("/Users/nadja/PycharmProjects/Tools/Aufgabe 11/data/test.txt",
+            "/Users/nadja/PycharmProjects/Tools/Aufgabe 11/data/dev.txt")
+
+sample = data.train_parses[0]
+suffix_tensor, prefix_tensor = data.words2charIDvec(sample[0])
+suffix_tensor = torch.Tensor(suffix_tensor)
+prefix_tensor = torch.Tensor(prefix_tensor)
+parser(suffix_tensor, prefix_tensor)
+
+
