0ec9b3411bf91964b4d3f27c8873c98026e06c61
nseeberg <nadja.seeberg@web.de>
2022-02-02 11:10:42 +0100

    first implementation of build_parse/ output

--- Aufgabe 12/parser-analyze.py
+++ Aufgabe 12/parser-analyze.py
@@ -8,6 +8,7 @@ from parser import Parser
 from Data import Data
 from utils import load_config, get_sentences_from_test, load_from_pickle, load_data, build_labels_vector
 
+
 class Analyzer:
 
     def __init__(self, config):
@@ -31,23 +32,23 @@ class Analyzer:
         sample = self.test_data[3]
         suffix, prefix = self.data_class.words2charIDvec(sample, self.device)
         logits = self.model(prefix, suffix)
-        argmax_label_ids =  torch.argmax(logits, dim=1)
+        argmax_label_ids = torch.argmax(logits, dim=1)
         best_labels = [self.data_class.ID2label[i] for i in argmax_label_ids]
         best_scores = torch.max(logits, dim=1)[0]
         n = len(sample)
-        #max_index = len(best_labels)
-        #split = [0] * max_index
+        # max_index = len(best_labels)
+        # split = [0] * max_index
         labels_vector = build_labels_vector(n)
+        best_indices = None
 
         for l in range(1, n):
-            for i in range(n-l+1):
+            for i in range(n - l + 1):
                 if l > 1:
 
                     # Compute argmax best split
-                    best_indices = None
                     best_score = float("-inf")
-                    for j in range(i+1, n):
-                        for k in range(j+1, n+1):
+                    for j in range(i + 1, n):
+                        for k in range(j + 1, n + 1):
                             ij_index = labels_vector.index(j - i) + i
                             jk_index = labels_vector.index(k - j) + j
                             ij_score = float(best_scores[ij_index])
@@ -55,7 +56,19 @@ class Analyzer:
                             new_score = ij_score + jk_score
                             if new_score > best_score:
                                 best_score = new_score
-                                best_indices = (i, k)
+                                best_indices = i, k
+        self.output(best_indices[0], best_indices[1], best_labels, sample)
+
+    def output(self, i, k, best_labels, test_sentence):
+        sentences = test_sentence
+        labels = [label for label in best_labels if label != '<unk>']
+        print("(", labels[k-i])
+        if k == i+1:
+            print(test_sentence[i])
+        else:
+            self.output(i, k-i, labels, sentences)
+            self.output(k-i, k, labels, sentences)
+        print(")")
 
 
 analyzer_config = {"model_config_path": "./Run-5/configs.txt",
